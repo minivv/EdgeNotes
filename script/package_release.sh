580bcdea3595e9a8a4417e5a08ec2d5b0a50a859
awei <1312461553@qq.com>
2026-07-06 16:02:35 +0800

    Clean release zip packaging

--- script/package_release.sh
+++ script/package_release.sh
@@ -11,7 +11,7 @@ ZIP_PATH="$DIST_DIR/$APP_NAME-macOS.zip"
 
 rm -rf "$DIST_DIR"
 mkdir -p "$DIST_DIR"
-ditto -c -k --keepParent "$APP_BUNDLE" "$ZIP_PATH"
+COPYFILE_DISABLE=1 ditto -c -k --keepParent --norsrc "$APP_BUNDLE" "$ZIP_PATH"
 shasum -a 256 "$ZIP_PATH" > "$ZIP_PATH.sha256"
 
 echo "$ZIP_PATH"
